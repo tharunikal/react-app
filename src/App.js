796bb946e054af13da11726bc2abf542df4f08d0
Tharunika L <tharunika.l14@gmail.com>
2024-11-07 15:12:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World, this is Tharunika L my roll number is 22CB059
+        </p>
+        <p>
+          III CSBS
+        </p>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
